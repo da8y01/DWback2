e16efd2fbb92e507fcd870ab4432223f6c923d18
Developer <developer@local.host>
2022-12-29 15:36:24 -0500

    Agregar y registrar database context.

---
+++ Models/ReminderContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Reminders1.Models
+{
+    public class ReminderContext : DbContext
+    {
+        public ReminderContext(DbContextOptions<ReminderContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ReminderItem> ReminderItems { get; set; } = null!;
+    }
+}
