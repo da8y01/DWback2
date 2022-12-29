0f862b84173787d8aec79a628f99ee89611304e9
Developer <developer@local.host>
2022-12-29 15:26:28 -0500

    Crear app, cambiar configuración launchUrl, agregar model ReminderItem.

---
+++ Models/ReminderItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Reminders1.Models
+{
+    public class ReminderItem
+    {
+        public long Id { get; set; }
+        public string? Description { get; set; }
+        public string? Email { get; set; }
+        //public bool IsComplete { get; set; }
+        public DateTime EventDate { get; set; }
+        public int? EventPriority { get; set; }
+    }
+}
